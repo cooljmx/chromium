eb922e8ef3767d842815c61b1ad45d0b4963b4c8
Alex <a.rttr@yandex.ru>
2022-04-03 23:31:50 +0300

    remove infinite loop, remove stdio headers, add sleep

--- algo/win/broker/host.cc
+++ algo/win/broker/host.cc
@@ -1,11 +1,6 @@
 #include <tchar.h>
 #include <windows.h>
 
-#include <io.h>
-#include <fcntl.h>
-#include "stdio.h"
-#include "stdlib.h"
-
 #include <iostream>
 #include <string>
 
@@ -15,7 +10,7 @@
 int run_broker_main(int argc, wchar_t** argv);
 
 int _tmain(int argc, wchar_t* argv[]) {
-    Sleep(10 * 1000);
+//  Sleep(10 * 1000);
     if (argc > 1) {
         std::cerr << "host" << std::endl;
         return host_main(argc, argv);
@@ -36,29 +31,28 @@ int run_broker_main(int argc, wchar_t** argv) {
     }
 
     std::ios_base::sync_with_stdio(false);
-    for (;;) {
-        for (std::string line; std::getline(std::cin, line);) {
-            // parse
-            algo::TargetInformation* target_result = new algo::TargetInformation;
-            algo::TargetOptions* options = new algo::TargetOptions{
-                exe,                                              // host_path
-                L"target, 0B90B2ED-7DBA-4FD6-B17E-C47533300557",  // command_line
-                L"test_env",                                      // package_name
-                L"",                                              // file rules
-                L"",                                              // reg_rules
-                L"",                                              // np_rules
-                L"",                                              // ev_rules
-            };
+    for (std::string line; std::getline(std::cin, line);) {
+        // parse
+        algo::TargetInformation* target_result = new algo::TargetInformation;
+        algo::TargetOptions* options = new algo::TargetOptions{
+            exe,                                              // host_path
+            L"target, 0B90B2ED-7DBA-4FD6-B17E-C47533300557",  // command_line
+            L"test_env",                                      // package_name
+            L"",                                              // file rules
+            L"",                                              // reg_rules
+            L"",                                              // np_rules
+            L"",                                              // ev_rules
+        };
 
-            Spawn(options, target_result);
+        Spawn(options, target_result);
 
-            if (target_result != nullptr) {
-                std::cout << target_result->process_id << " "
-                    << target_result->thread_id;
-                if (!Resume(target_result)) {
-                    std::cerr << "Resume target has failed" << std::endl;
-                }
+        if (target_result != nullptr) {
+            std::wcout << target_result->process_id << " " << target_result->thread_id << std::endl;
+            if (!Resume(target_result)) {
+                std::cerr << "Resume target has failed" << std::endl;
             }
         }
     }
+    Sleep(200);
+    return 0;
 }
