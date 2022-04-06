ffd896c3c78ccd404eb9e942af869b50d7ef34bc
Alex <a.rttr@yandex.ru>
2022-04-06 17:15:16 +0300

    convert UTF16 to UTF8

--- algo/win/broker/host.cc
+++ algo/win/broker/host.cc
@@ -7,7 +7,7 @@
 #include "algo/win/broker/algobroker.h"
 #include "algo/win/host/algohost.h"
 #include "base/json/json_reader.h"
-
+#include "base/strings/utf_string_conversions.h"
 
 int run_broker_main(int argc, wchar_t** argv);
 
@@ -34,13 +34,25 @@ int run_broker_main(int argc, wchar_t** argv) {
 
     std::ios_base::sync_with_stdio(false);
     for (std::string line; std::getline(std::cin, line);) {
-        base::Optional<base::Value> root = base::JSONReader::Read(line);
+        std::cerr << "line size is " << line.size() << std::endl;
+
+        const auto wline = std::wstring( (wchar_t*)line.data(), line.size() / 2);
+        std::cerr << "wline size is " << wline.size() << std::endl;
+
+        std::string output;
+        if (!base::UTF16ToUTF8(wline.c_str(), wline.size(), &output)) {
+            std::cerr << "Couldn't convert UTF16 to UTF8" << std::endl;
+            return -2;
+        }
+
+        const auto& narrow_line = output;
+        base::Optional<base::Value> root = base::JSONReader::Read(narrow_line);
         if (!root || root == base::nullopt) {
-            std::cerr << "Bad JSON: " << line << std::endl;
+            std::cerr << "Bad JSON: " << narrow_line << std::endl;
             continue;
         }
-        std::string *targetId = root->FindStringKey("targetId");
-        std::cerr << &targetId << std::endl;
+        const std::string *target_id = root->FindStringKey("targetId");
+        std::cerr << "target_id is " << &target_id << std::endl;
 
         algo::TargetInformation* target_result = new algo::TargetInformation;
         algo::TargetOptions* options = new algo::TargetOptions{
